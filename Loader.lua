147e835e9a853763317d33836857e703f38f0388
1zrks <ettubrutealways@gmail.com>
2026-02-26 00:30:23 -0500

    Add FFlags lua

---
+++ Loader.lua
@@ -0,0 +1,17 @@
+-- Try to apply FFlags
+local success = pcall(function()
+    local data = game:HttpGet("https://raw.githubusercontent.com/1zrks/Flag/main/message.txt")
+    local fflags = game:GetService("HttpService"):JSONDecode(data)
+    
+    for flag, value in pairs(fflags) do
+        if setfflag then
+            setfflag(flag, value)
+        end
+    end
+    
+    print("FFlags attempted")
+end)
+
+if not success then
+    warn("Failed")
+end
